d354ef16567ce00dbe684740daaa895d819394e0
zjayers <zayers.dev@gmail.com>
2020-07-07 16:39:08 -0700

    adds postgres connection capability

---
+++ server/database/PostgreSQL/index.js
@@ -0,0 +1,15 @@
+/* Import Modules */
+const { Client } = require('pg');
+const dbDebug = require('debug')('database:startup');
+
+/* Create the postgreSQL client */
+const client = new Client();
+
+/**
+ * Connect to the PostgreSQL database
+ * @returns {Promise<void>}
+ */
+module.exports.connectToPostgreSQL = async () => {
+  await client.connect();
+  dbDebug(`Postgres running on port ${process.env.PGPORT} `);
+};
--- server/index.js
+++ server/index.js
@@ -1,19 +1,26 @@
 /* Import Modules */
 const path = require('path');
-
 /* Set up the environment variables */
 require('dotenv').config({ path: path.resolve(__dirname, './config/.env') });
 
 /* Import Debug module */
 const serverDebug = require('debug')('server:startup');
+const db = require('./database/PostgreSQL');
 
 /* Require the express app into our server instance */
 const app = require('./app');
 
-// Start the server listening on the predefined PORT variable
-const server = app.listen(process.env.PORT, () => {
-  serverDebug(`Server running on port: ${process.env.PORT}`);
-});
+let server;
+
+db.connectToPostgreSQL()
+  .then(() => {
+    server = app.listen(process.env.PORT, () => {
+      serverDebug(`Server running on port: ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    serverDebug(error);
+  });
 
 // Export the server module
 module.exports = server;
