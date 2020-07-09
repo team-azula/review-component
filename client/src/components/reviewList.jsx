3311fe69c8ed1713d62b3436ba6cc9eb6c0f8c8d
zjayers <zayers.dev@gmail.com>
2020-07-09 00:10:50 -0700

    refactors problem areas of front end to work as expected, renames "_id" identifier on reviews to "id"

--- client/src/components/reviewList.jsx
+++ client/src/components/reviewList.jsx
@@ -1,16 +1,16 @@
-import React from "react";
-import ReviewItem from "./reviewItem.jsx";
+import React from 'react';
+import ReviewItem from './reviewItem.jsx';
 
 const ReviewList = function (props) {
   const reviews = props.reviews.map((review) => {
-    return <ReviewItem key={review._id} review={review} />;
+    return <ReviewItem key={review.id} review={review} />;
   });
 
   if (!reviews.length) {
     reviews.push(
       <p
         key="empty"
-        style={{ padding: "30px 0", textAlign: "center", fontSize: "160%" }}
+        style={{ padding: '30px 0', textAlign: 'center', fontSize: '160%' }}
       >
         Sorry, It doesn't seem like there are any reviews.
       </p>
@@ -18,7 +18,7 @@ const ReviewList = function (props) {
   }
 
   return (
-    <div style={{ backgroundColor: "white", padding: "10px 40px 0 45px" }}>
+    <div style={{ backgroundColor: 'white', padding: '10px 40px 0 45px' }}>
       {reviews}
     </div>
   );
