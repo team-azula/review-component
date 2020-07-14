a2316bd03c52f68ba1a3bf51ec1ce7e6c8168374
Zach Ayers <ayers@Zachs-Mac-mini.local>
2020-07-13 21:58:11 -0500

    pre-deploy

--- client/src/components/reviewItem.jsx
+++ client/src/components/reviewItem.jsx
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
-import $ from 'jquery';
+import React, { Component } from "react";
+import $ from "jquery";
 
 class ReviewItem extends Component {
   constructor(props) {
     super(props);
     this.state = {
       review: props.review,
-      stars: []
+      stars: [],
     };
   }
 
@@ -29,7 +29,7 @@ class ReviewItem extends Component {
       stars.push(<i key={i} className="icon-star-empty"></i>);
     }
     this.setState({
-      stars
+      stars,
     });
   }
 
@@ -37,33 +37,61 @@ class ReviewItem extends Component {
     const review = this.state.review;
     if ($.post) {
       $.post({
-        url: `http://localhost:3002/likes/${review._id}`
-      })
-        .then(()=> {
-          review.likes++;
-          this.setState({
-            review
-          });
+        url: `http://ec2-3-136-25-220.us-east-2.compute.amazonaws.com:3002/likes/${review._id}`,
+      }).then(() => {
+        review.likes++;
+        this.setState({
+          review,
         });
+      });
     }
   }
 
   render() {
     return (
-      <div style={{padding: '15px 20px 25px'}}>
+      <div style={{ padding: "15px 20px 25px" }}>
         <div className="row">
-          <div className="col-md-1" style={{padding: '0 0 0'}}>
-            <p style={{backgroundColor: 'lightcoral', width: '55px', height: '55px', borderRadius: '50%', display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '160%', fontWeight: '500', color: 'white'}}>{this.state.review.author[0]}</p>
+          <div className="col-md-1" style={{ padding: "0 0 0" }}>
+            <p
+              style={{
+                backgroundColor: "lightcoral",
+                width: "55px",
+                height: "55px",
+                borderRadius: "50%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                fontSize: "160%",
+                fontWeight: "500",
+                color: "white",
+              }}
+            >
+              {this.state.review.author[0]}
+            </p>
           </div>
-          <div className="col-md-11" style={{padding: '0 0 0 25px'}}>
+          <div className="col-md-11" style={{ padding: "0 0 0 25px" }}>
             <div className="row">
               <div className="col-sm-10 col-md-9">
-                <p style={{fontSize: '125%', fontWeight: '500'}}>{this.state.review.author}</p>
-                <p style={{fontSize: '14px', letterSpacing: '2px'}}>{this.state.stars}</p>
+                <p style={{ fontSize: "125%", fontWeight: "500" }}>
+                  {this.state.review.author}
+                </p>
+                <p style={{ fontSize: "14px", letterSpacing: "2px" }}>
+                  {this.state.stars}
+                </p>
               </div>
-              <p className="col-sm-2 col-md-3" style={{fontSize: '120%', fontWeight: '200'}}><a onClick={this.handleLikeClick.bind(this)}><i className="icon-thumbs-up"></i></a>: {this.state.review.likes}</p>
+              <p
+                className="col-sm-2 col-md-3"
+                style={{ fontSize: "120%", fontWeight: "200" }}
+              >
+                <a onClick={this.handleLikeClick.bind(this)}>
+                  <i className="icon-thumbs-up"></i>
+                </a>
+                : {this.state.review.likes}
+              </p>
             </div>
-            <p style={{fontSize: '18px', fontWeight: '300'}}>{this.state.review.body}</p>
+            <p style={{ fontSize: "18px", fontWeight: "300" }}>
+              {this.state.review.body}
+            </p>
           </div>
         </div>
       </div>
